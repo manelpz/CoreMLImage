ddc5864f7c7d83941d11d4977748282ca2d504fc
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-07-12 23:32:29 -0500

    Initial Commit 🚀

---
+++ CoreMLImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreMLImage
+//
+//  Created by Emmanuel Lopez Guerrero on 12/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
